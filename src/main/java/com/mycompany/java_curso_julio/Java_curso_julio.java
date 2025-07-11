5fefb8aaf51a546e26b564b96190ebc50e9e163a
ALP300 <aitorpalomino1@gmail.com>
2025-07-10 19:08:22 -0500

    Subir Proyecto

---
+++ src/main/java/com/mycompany/java_curso_julio/Java_curso_julio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java_curso_julio;
+
+/**
+ *
+ * @author aitor
+ */
+public class Java_curso_julio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
